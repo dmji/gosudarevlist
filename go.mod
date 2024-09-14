bdf47e142dcedc6257d9fbc3bdff4f7ed0d8c6e9
dmji <leyshkinanton@gmail.com>
2024-09-15 00:18:18 +0300

    init zap logger

--- go.mod
+++ go.mod
@@ -5,8 +5,12 @@ go 1.23.0
 require (
 	github.com/a-h/templ v0.2.747
 	github.com/lithammer/fuzzysearch v1.1.8
+	go.uber.org/zap v1.27.0
 	golang.org/x/net v0.28.0
 	golang.org/x/sync v0.8.0
 )
 
-require golang.org/x/text v0.17.0 // indirect
+require (
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+)
---
+++ pkg/logger/logger.go
@@ -0,0 +1,85 @@
+package logger
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type loggerCtx string
+
+const (
+	loggerCtxValue loggerCtx = "logger"
+)
+
+type loggerZap struct {
+	logger *zap.SugaredLogger
+}
+
+var globalLogger *loggerZap
+
+func New() (*loggerZap, error) {
+	loggerConfig := zap.NewProductionConfig()
+	loggerConfig.Level.SetLevel(zapcore.DebugLevel)
+	loggerConfig.ErrorOutputPaths = []string{"stdout"}
+	loggerConfig.EncoderConfig.MessageKey = zapcore.OmitKey
+
+	logger, err := loggerConfig.Build()
+	if err != nil {
+		return nil, fmt.Errorf("loggerConfig.Build: %w", err)
+	}
+
+	once := sync.Once{}
+	once.Do(func() {
+		globalLogger = &loggerZap{logger: logger.Sugar()}
+	})
+
+	return &loggerZap{logger: logger.Sugar()}, nil
+}
+
+func ToContext(ctx context.Context, logger *loggerZap) context.Context {
+	return context.WithValue(ctx, loggerCtxValue, logger)
+}
+
+func Infow(ctx context.Context, msg string, keysAndValues ...interface{}) {
+	if loggerC, ok := ctx.Value(loggerCtxValue).(*loggerZap); ok {
+		loggerC.logger.Infow(msg, keysAndValues...)
+
+		return
+	}
+
+	globalLogger.logger.Infow(msg, keysAndValues...)
+}
+
+func Errorw(ctx context.Context, msg string, keysAndValues ...interface{}) {
+	if loggerC, ok := ctx.Value(loggerCtxValue).(*loggerZap); ok {
+		loggerC.logger.Errorw(msg, keysAndValues...)
+
+		return
+	}
+
+	globalLogger.logger.Errorw(msg, keysAndValues...)
+}
+
+func Panicw(ctx context.Context, msg string, keysAndValues ...interface{}) {
+	if loggerC, ok := ctx.Value(loggerCtxValue).(*loggerZap); ok {
+		loggerC.logger.Panicw(msg, keysAndValues...)
+
+		return
+	}
+
+	globalLogger.logger.Panicw(msg, keysAndValues...)
+}
+
+func Fatalw(ctx context.Context, msg string, keysAndValues ...interface{}) {
+	if loggerC, ok := ctx.Value(loggerCtxValue).(*loggerZap); ok {
+		loggerC.logger.Fatalw(msg, keysAndValues...)
+
+		return
+	}
+
+	globalLogger.logger.Panicw(msg, keysAndValues...)
+}
