c83cbb2a4349b272b614ee0ba1443c9220e0d9a0
dmji <leyshkinanton@gmail.com>
2024-09-16 03:32:58 +0300

    ++

--- internal/services/cart_generator.go
+++ internal/services/cart_generator.go
@@ -20,8 +20,12 @@ func queryEncodeForMyAnimeList(name string) string {
 	return params.Encode()
 }
 
-func queryPosterFromItem(item *model.AnimeLayerItem) string {
-	_ = item
+func queryPosterFromItem(description *model.AnimeLayerItemDescription) string {
+
+	if img := description.RefImageCover; len(img) > 0 {
+		return img
+	}
+
 	return "/assets/no_image.jpg"
 }
 
@@ -38,13 +42,23 @@ func (s *services) GenerateCards(ctx context.Context, page int) []components.Ite
 	cards := make([]components.ItemCartData, 0, perPage)
 	for id := startID; id < endID; id++ {
 		item := &items[id-startID]
+
+		description, _ := s.AnimeLayerRepositoryDriver.GetDescription(ctx, item.GUID)
+
+		descStr := ""
+		for _, v := range description.Descriptions {
+			switch v.Key {
+			case "Разрешение", "Жанр":
+				descStr = descStr + v.Key + ": " + v.Value
+			}
+		}
+
 		cards = append(cards, components.ItemCartData{
-			ID:             id + 1,
-			Title:          item.Name,
-			Image:          queryPosterFromItem(item),
-			Description:    "Test text",
-			AnimeLayerRef:  fmt.Sprintf("https://animelayer.ru/torrent/%s/", item.GUID),
-			MyAnimeListRef: fmt.Sprintf("https://myanimelist.net/search/all?%s", queryEncodeForMyAnimeList(item.Name)),
+			ID:            id + 1,
+			Title:         item.Name,
+			Image:         queryPosterFromItem(&description),
+			Description:   descStr,
+			AnimeLayerRef: fmt.Sprintf("https://animelayer.ru/torrent/%s/", item.GUID),
 		})
 	}
 	return cards
