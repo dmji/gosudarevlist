3491a00dc218f970fd657c0787e05421e6db8267
dmji <leyshkinanton@gmail.com>
2024-10-01 02:31:43 +0300

    improved repo for pgx

--- pkg/animelayer/repository/repository.go
+++ pkg/animelayer/repository/repository.go
@@ -2,14 +2,28 @@ package animelayer_repository
 
 import (
 	sqlc "collector/pkg/animelayer/repository/sqlc"
+	"context"
+
+	"github.com/jackc/pgx/v5"
 )
+
+type txStarter interface {
+	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
+}
+
+type sqlDriver interface {
+	sqlc.DBTX
+	txStarter
+}
 
 type repository struct {
 	query *sqlc.Queries
+	db    sqlDriver
 }
 
-func NewRepository(db sqlc.DBTX) *repository {
+func NewRepository(db sqlDriver) *repository {
 	return &repository{
+		db:    db,
 		query: sqlc.New(db),
 	}
 }
