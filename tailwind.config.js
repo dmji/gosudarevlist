86782bb0e503e943039d0445d0b218106a1674c2
dmji <leyshkinanton@gmail.com>
2025-01-04 16:13:25 +0300

    ++

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./components/**/*.templ"],
+  content: ["./components/**/*.templ", "./components/*.templ"],
   safelist: [
     'w-64',
     'w-1/2',
