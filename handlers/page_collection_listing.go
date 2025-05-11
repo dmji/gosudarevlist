b8dd9e0ba9d40049aea08032068cd6cb9f3c638e
leyshkinanton@gmail.com <leyshkinanton@gmail.com>
2025-05-12 00:14:33 +0300

    hide upper categories navbar

--- handlers/page_collection_listing.go
+++ handlers/page_collection_listing.go
@@ -10,12 +10,12 @@ import (
 	"github.com/dmji/gosudarevlist/pkg/logger"
 )
 
-var categories = []enums.Category{
-	enums.CategoryAnime,
-	enums.CategoryManga,
-	enums.CategoryDorama,
-	enums.CategoryMusic,
-}
+//var categories = []enums.Category{
+//	enums.CategoryAnime,
+//	enums.CategoryManga,
+//	enums.CategoryDorama,
+//	enums.CategoryMusic,
+//}
 
 func (router *router) CollectionListingPageHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
@@ -42,19 +42,19 @@ func (router *router) CollectionListingPageHandler(w http.ResponseWriter, r *htt
 
 	logger.Infow(ctx, "Handler | ShelfPageHandler", "params", nextPageParams)
 
-	categoriesLine := make([]model.CategoryButton, 0, len(categories))
-	for _, c := range categories {
-		categoriesLine = append(categoriesLine, model.NewCategoryButton(c,
-			"/animelayer/%s",
-			"/animelayer/%s/updates",
-			c == cat,
-		))
-	}
+	//categoriesLine := make([]model.CategoryButton, 0, len(categories))
+	//for _, c := range categories {
+	//	categoriesLine = append(categoriesLine, model.NewCategoryButton(c,
+	//		"/animelayer/%s",
+	//		"/animelayer/%s/updates",
+	//		c == cat,
+	//	))
+	//}
 
 	err = pages.CollectionListing(
 		"/api/filters/"+cat.String(),
 		"/api/cards/"+cat.String(),
-		categoriesLine,
+		nil, // categoriesLine
 	).Render(r.Context(), w)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
